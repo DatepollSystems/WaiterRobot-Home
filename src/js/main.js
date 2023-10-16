b5270b3f6581aa5904050b9bd37bc53b1bdf7304
Dafnik <git@dafnik.me>
2023-10-16 12:46:18 +0200

    fix: mobile optimizations, add carousel

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+import '../assets/styles.scss'
+import '../assets/carousel.scss'
+// noinspection ES6UnusedImports
+import Carousel from 'bootstrap/js/dist/carousel';
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (let i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+// JavaScript code for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+
+            // Update the URL without triggering a full page reload
+            history.pushState({}, '', `#${targetId}`);
+        }
+    });
+});

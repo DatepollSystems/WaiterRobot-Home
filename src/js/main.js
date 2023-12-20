57cc6fd505fa584a0499ff3e53298020d034c49f
Fabian Schedler <dev.fabian.schedler@gmail.com>
2023-12-20 23:04:40 +0100

    Fix carousel

--- src/js/main.js
+++ src/js/main.js
@@ -3,40 +3,36 @@ import "../assets/animate.scss";
 import "../assets/carousel.scss";
 // noinspection ES6UnusedImports
 import Carousel from "bootstrap/js/dist/carousel";
+import $ from "jquery"
 
-function applyMultiItemCarousel(items, minPerSlide) {
-  items.forEach((el) => {
-    let next = el.nextElementSibling;
+$('#eventCarousel .carousel-item').each(function () {
+    let minPerSlide = 4;
+    let next = $(this).next();
     for (let i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        // wrap carousel by using first child
-        next = items[0];
-      }
-      let cloneChild = next.cloneNode(true);
-      el.appendChild(cloneChild.children[0]);
-      next = next.nextElementSibling;
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        next = next.next();
     }
-  });
-}
-
-applyMultiItemCarousel(document.querySelectorAll("#eventsCarousel .carousel-item"), 4)
+});
 
 // JavaScript code for smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
 
-    const targetId = this.getAttribute("href").substring(1);
-    const targetElement = document.getElementById(targetId);
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
 
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: "smooth",
-      });
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth",
+            });
 
-      // Update the URL without triggering a full page reload
-      history.pushState({}, "", `#${targetId}`);
-    }
-  });
+            // Update the URL without triggering a full page reload
+            history.pushState({}, "", `#${targetId}`);
+        }
+    });
 });
